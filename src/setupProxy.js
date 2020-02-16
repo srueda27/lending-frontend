01b5e2983825613226e12620c69b8f345c3078db
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-02-16 17:42:33 -0500

    add setupProxy file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,9 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        proxy(["/api", , "/otherApi"], { target: "https://shielded-brushlands-56249.herokuapp.com" })
+        proxy("/puppy/Sammy", {
+            target: "https://shielded-brushlands-56249.herokuapp.com",
+            changeOrigin: true
+        })
     );
 };
